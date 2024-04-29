f18eae36250703dbda2ded544b5642cd45deae8c
igor <igsantos1996@gmail.com>
2024-04-29 16:22:41 -0300

    start with Gemini API

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import {GoogleGenerativeAI} from "@google/generative-ai"
+
+config();
+const API_KEY = process.env.API_KEY;
+
+const genAI = new GoogleGenerativeAI(API_KEY ?? '');
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const prompt = "what kind of projects can I use Gemini?";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
